4a3c08bbecf175a2fae79c2035dde5e17f9812f0
Florent Le Gall <florent.legall@datadoghq.com>
2022-02-25 13:46:13 +0100

    Clarify constraint exercice

--- src/01-type-constraints/exercise.ts
+++ src/01-type-constraints/exercise.ts
@@ -1,11 +1,15 @@
 import { TODO } from "../helpers";
 
-const increment = (value: TODO): TODO => {
+/**
+ * We would like our increment function to accept all kind of parameters
+ * which have at least a {count: number} property
+ */
+const increment = <T extends TODO>(value: T): T => {
   value.count++;
   return value;
 };
 
 const x = increment({ count: 1, type: "voiture" as const });
 const y = increment({ count: 2, type: "velo" as const });
-// @ts-expect-error
+// @ts-expect-error no count property on parameter
 const z = increment({ type: "moto" as const });
