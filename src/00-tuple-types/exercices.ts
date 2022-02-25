d817a86791b7d2f52f5b3ac91da1eb22f38bac38
Florent Le Gall <florent.legall@datadoghq.com>
2022-02-25 13:45:25 +0100

    Add folders for basic types exercices

---
+++ src/00-tuple-types/exercices.ts
@@ -0,0 +1,6 @@
+// TODO introduce after conditional types
+// Extract head with extends [infer head, ...]
+// Extract tail with extends [..., infer tail]
+
+// Bonus
+// Maybe give head solution using tuple[0] and ask about implementing tail with a similar approach
