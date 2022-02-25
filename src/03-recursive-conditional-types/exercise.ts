e560550a31ca3d8b58e63f1d5b4f8f78dad2d05b
Florent Le Gall <florent.legall@datadoghq.com>
2022-02-25 13:48:32 +0100

    Add exercise on recursive UnwrapPromise

--- src/03-recursive-conditional-types/exercise.ts
+++ src/03-recursive-conditional-types/exercise.ts
@@ -5,35 +5,58 @@ import { Equal, Expect, Tuple, TODO } from "../helpers";
  */
 
 /**
+ * ## Recusive UnwrapPromise
+ *
+ * Copy the UnwrapPromise implementation from the condition type exercise and
+ * improve it to handle recursive Promise<Promise<T>>
+ */
+type UnwrapPromise<input> = TODO;
+
+namespace exercise_1 {
+  type res1 = UnwrapPromise<"NOT A PROMISE">;
+  type test1 = Expect<Equal<res1, "NOT A PROMISE">>;
+
+  type res2 = UnwrapPromise<Promise<"Hello">>;
+  type test2 = Expect<Equal<res2, "Hello">>;
+
+  type res3 = UnwrapPromise<Promise<Promise<"Hello">>>;
+  type test3 = Expect<Equal<res3, "Hello">>;
+
+  type res4 = UnwrapPromise<Promise<Promise<Promise<"Hello">>>>;
+  type test4 = Expect<Equal<res4, "Hello">>;
+}
+
+/**
  * Write a All<Promises> type which turns an array of Promises of value
  * into a Promise of arrays of values.
  *
  * All<[Promise<number>, Promise<string>, Promise<boolean>]> -> [number, string, boolean]
  */
+type All<promises> = TODO;
 
-type All<Promises> = TODO;
+namespace exercise_2 {
+  type res1 = All<[Promise<number>]>;
+  type test1 = Expect<Equal<res1, [number]>>;
 
-type res1 = All<[Promise<number>]>;
-type test1 = Expect<Equal<res1, [number]>>;
+  type res2 = All<[Promise<number>, Promise<string>, Promise<boolean>]>;
+  type test2 = Expect<Equal<res2, [number, string, boolean]>>;
 
-type res2 = All<[Promise<number>, Promise<string>, Promise<boolean>]>;
-type test2 = Expect<Equal<res2, [number, string, boolean]>>;
+  type res3 = All<[Promise<number>, Promise<string>]>;
+  type test3 = Expect<Equal<res3, [number, string]>>;
 
-type res3 = All<[Promise<number>, Promise<string>]>;
-type test3 = Expect<Equal<res3, [number, string]>>;
+  type res4 = All<[]>;
+  type test4 = Expect<Equal<res4, []>>;
 
-type res4 = All<[]>;
-type test4 = Expect<Equal<res4, []>>;
+  function all<Promises extends Tuple>(promises: Promises): All<Promises> {
+    return Promise.all(promises) as unknown as All<Promises>;
+  }
 
-function all<Promises extends Tuple>(promises: Promises): All<Promises> {
-  return Promise.all(promises) as unknown as All<Promises>;
+  const res5 = all([
+    Promise.resolve(2),
+    Promise.resolve("string"),
+    Promise.resolve(true),
+  ]);
+
+  type res5 = typeof res5;
+  type test5 = Expect<Equal<res5, [number, string, boolean]>>;
 }
-
-const res5 = all([
-  Promise.resolve(2),
-  Promise.resolve("string"),
-  Promise.resolve(true),
-]);
-
-type res5 = typeof res5;
-type test5 = Expect<Equal<res5, [number, string, boolean]>>;
--- src/03-recursive-conditional-types/solution.ts
+++ src/03-recursive-conditional-types/solution.ts
@@ -1,41 +1,65 @@
-import { Equal, Expect, Tuple } from "../helpers";
+import { Equal, Expect, Tuple, TODO } from "../helpers";
 
 /**
  * ONE
  */
 
 /**
- * Write a All<Promises> type which turns an array of Promises of value
- * into a Promise of arrays of values.
+ * ## Recusive UnwrapPromise
+ *
+ * Copy the UnwrapPromise implementation from the condition type exercise and
+ * improve it to handle recursive Promise<Promise<T>>
  */
+type UnwrapPromise<input> = input extends Promise<infer awaited>
+  ? UnwrapPromise<awaited>
+  : input;
+namespace exercise_1 {
+  type res1 = UnwrapPromise<"NOT A PROMISE">;
+  type test1 = Expect<Equal<res1, "NOT A PROMISE">>;
 
-type UnwrapPromise<T> = T extends Promise<infer V> ? V : never;
+  type res2 = UnwrapPromise<Promise<"Hello">>;
+  type test2 = Expect<Equal<res2, "Hello">>;
 
-type All<Promises> = Promises extends [infer head, ...infer rest]
-  ? [UnwrapPromise<head>, ...All<rest>]
-  : [];
+  type res3 = UnwrapPromise<Promise<Promise<"Hello">>>;
+  type test3 = Expect<Equal<res3, "Hello">>;
 
-type res1 = All<[Promise<number>]>;
-type test1 = Expect<Equal<res1, [number]>>;
-
-type res2 = All<[Promise<number>, Promise<string>, Promise<boolean>]>;
-type test2 = Expect<Equal<res2, [number, string, boolean]>>;
-
-type res3 = All<[Promise<number>, Promise<string>]>;
-type test3 = Expect<Equal<res3, [number, string]>>;
-
-type res4 = All<[]>;
-type test4 = Expect<Equal<res4, []>>;
-
-function all<Promises extends Tuple>(promises: Promises): All<Promises> {
-  return Promise.all(promises) as unknown as All<Promises>;
+  type res4 = UnwrapPromise<Promise<Promise<Promise<"Hello">>>>;
+  type test4 = Expect<Equal<res4, "Hello">>;
 }
 
-const res5 = all([
-  Promise.resolve(2),
-  Promise.resolve("string"),
-  Promise.resolve(true),
-]);
+/**
+ * Write a All<Promises> type which turns an array of Promises of value
+ * into a Promise of arrays of values.
+ *
+ * All<[Promise<number>, Promise<string>, Promise<boolean>]> -> [number, string, boolean]
+ */
+type All<promises> = promises extends [infer head, ...infer tail]
+  ? [UnwrapPromise<head>, ...All<tail>]
+  : [];
 
-type res5 = typeof res5;
-type test5 = Expect<Equal<res5, [number, string, boolean]>>;
+namespace exercise_2 {
+  type res1 = All<[Promise<number>]>;
+  type test1 = Expect<Equal<res1, [number]>>;
+
+  type res2 = All<[Promise<number>, Promise<string>, Promise<boolean>]>;
+  type test2 = Expect<Equal<res2, [number, string, boolean]>>;
+
+  type res3 = All<[Promise<number>, Promise<string>]>;
+  type test3 = Expect<Equal<res3, [number, string]>>;
+
+  type res4 = All<[]>;
+  type test4 = Expect<Equal<res4, []>>;
+
+  function all<Promises extends Tuple>(promises: Promises): All<Promises> {
+    return Promise.all(promises) as unknown as All<Promises>;
+  }
+
+  const res5 = all([
+    Promise.resolve(2),
+    Promise.resolve("string"),
+    Promise.resolve(true),
+  ]);
+
+  type res5 = typeof res5;
+  type test5 = Expect<Equal<res5, [number, string, boolean]>>;
+}
