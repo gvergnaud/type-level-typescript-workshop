562d1e64b3e291e2cbaf15bb9d1976673cebd32a
gvergnaud <gabriel.vergnaud@gmail.com>
2022-03-25 15:57:45 +0100

    Add exercises for union chapiter

--- 04-union-types/exercices.ts
+++ 04-union-types/exercices.ts
@@ -1,27 +1,54 @@
-/**
- * TODO:
- * - implement Exclude
- * - implement Extract
- * - transform a union of values into a union of structure
- * - type never: the empty union
- */
+import { Equal, Expect, TODO } from "../helpers";
 
+// 1. Implement a generic taking two union types and excluding one from the other
 namespace one {
-  /**
-   * TODO:
-   * - implement Exclude
-   * - implement Extract
-   * - transform a union of values into a union of structure?
-   * - type never: the empty union
-   */
+  type Exclude<a, b> = TODO;
+
+  type res1 = Exclude<1 | 2 | 3, 1>;
+  type test1 = Expect<Equal<res1, 2 | 3>>;
+
+  type res2 = Exclude<1 | 2 | 3, 1 | 2>;
+  type test2 = Expect<Equal<res2, 3>>;
+
+  type res3 = Exclude<1 | 2 | 3, never>;
+  type test3 = Expect<Equal<res3, 1 | 2 | 3>>;
+
+  type res4 = Exclude<1 | 2 | 3, unknown>;
+  type test4 = Expect<Equal<res4, never>>;
 }
 
+/**
+ * 2. Implement a generic taking a union type `a` and a second type parameter `b`,
+ *    and excluding types in `a` which aren't assignable to `b`
+ *    Only if they are contained in the second one.
+ */
 namespace two {
-  type ExpectTuple<tuple extends [] | [any, ...any]> = tuple;
+  type Extract<a, b> = TODO;
 
-  type res1 = ExpectTuple<[1, 2]>;
-  type res2 = ExpectTuple<[2]>;
-  type res3 = ExpectTuple<[]>;
-  // @ts-expect-error
-  type res4 = ExpectTuple<any[]>;
+  type res1 = Extract<1 | 2 | 3, 1>;
+  type test1 = Expect<Equal<res1, 1>>;
+
+  type unionOfObjects =
+    | { type: "a"; name: string }
+    | { type: "b"; name: string }
+    | { type: "c" };
+
+  type res2 = Extract<unionOfObjects, { type: "b" }>;
+  type test2 = Expect<Equal<res2, { type: "b"; name: string }>>;
+
+  type res3 = Extract<unionOfObjects, { name: string }>;
+  type test3 = Expect<
+    Equal<res3, { type: "a"; name: string } | { type: "b"; name: string }>
+  >;
+
+  type mixedUnion = 1 | 2 | { name: string } | [1] | [1, 2] | string[];
+
+  type res4 = Extract<mixedUnion, number>;
+  type test4 = Expect<Equal<res4, 1 | 2>>;
+
+  type res5 = Extract<mixedUnion, any[]>;
+  type test5 = Expect<Equal<res5, [1] | [1, 2] | string[]>>;
 }
+
+// 3. TODO: Union of literals to union of structure
+namespace three {}
--- 06-tuple-types/exercices.ts
+++ 06-tuple-types/exercices.ts
@@ -2,3 +2,21 @@
 // Extract head with extends [infer head, ...]
 // Extract tail with extends [..., infer tail]
 // Concatenate to tuples
+
+import { Equal, Expect, TODO } from "../helpers";
+
+namespace two {
+  type IsTuple<tuple> = TODO;
+
+  type res1 = IsTuple<[1, 2]>;
+  type test1 = Expect<Equal<res1, true>>;
+
+  type res2 = IsTuple<[2]>;
+  type test2 = Expect<Equal<res2, true>>;
+
+  type res3 = IsTuple<[]>;
+  type test3 = Expect<Equal<res3, true>>;
+
+  type res4 = IsTuple<any[]>;
+  type test4 = Expect<Equal<res4, false>>;
+}
