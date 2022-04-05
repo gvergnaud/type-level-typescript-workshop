7d82cbd833871821b4b03832c79ca02096c87319
Florent Le Gall <florent.legall@datadoghq.com>
2022-04-05 22:30:00 +0200

    Add exercise on assignability

--- 02-assignability/exercise.ts
+++ 02-assignability/exercise.ts
@@ -1,18 +1,7 @@
 import { Equal, Expect, TODO } from "../helpers";
 
-/**
- * TODO:
- * - assignability of literals and primitive types.
- * - assignability of arrays and tuples
- * - assignability of objects
- *    - extends keyof
- *    - optional keys?
- * - assignability of unions
- * - assignability of never and unknown, any
- * - assignability of functions
- * - constraints
- */
-namespace one {
+namespace strings {
+  // Add a constraint on the two functions types so that it accepts only a string or only 'a'
   function expectString<P extends TODO>(param: P) {
     return param;
   }
@@ -33,12 +22,12 @@ namespace one {
 
 // TODO objects assignability tests
 namespace objects {
-  /**
-   * We would like our increment function to accept all kind of parameters
-   * which have at least a {count: number} property
-   */
-  namespace five {
-    // Get rid of the any and fix it
+  namespace one {
+    /**
+     * We would like our increment function to accept all kind of parameters
+     * which have at least a {count: number} property.
+     * Get rid of the any and type this function
+     */
     const increment = (value: any): TODO => {
       value.count++;
       return value;
@@ -52,35 +41,51 @@ namespace objects {
 
     // @ts-expect-error: no count property on parameter
     const res3 = increment({ type: "moto" as const });
+
+    // @ts-expect-error: no count property on parameter
+    const res3 = increment({});
   }
 
   namespace two {
-    type Get<obj, key extends TODO> = TODO;
+    /**
+     * Let's write a Get type that takes an object type and a named property return the type of the named property
+     */
+    type Get<obj, key> = TODO;
 
-    // TODO
+    type res1 = Get<{ age: number }, "age">;
+    type test1 = Expect<Equal<res1, number>>;
+
+    type res2 = Get<{ firstName: string; lastName: string }, "firstName">;
+    type test2 = Expect<Equal<res2, string>>;
+
+    type res3 = Get<
+      { firstName: string; lastName: string },
+      // @ts-expect-error no age_of_the_captain property
+      "age_of_the_captain"
+    >;
   }
 }
 
-// TODO union assignability tests
 namespace unions {
-  namespace one {
-    function expectAOrB<P extends TODO>(param: P) {
-      return param;
-    }
-
-    const param1: "a" = "a";
-    expectAOrB(param1);
-
-    const param2: "b" = "b";
-    expectAOrB(param2);
-
-    const param3: "a" | "b" = "b";
-    expectAOrB(param3);
-
-    const param4: string = "b";
-    // @ts-expect-error
-    expectAOrB(param4);
+  /**
+   * We would like our expectAOrB accept 'a' or 'b' and return the same type as passed
+   */
+  function expectAOrB(param: TODO) {
+    return param;
   }
+
+  const param1: "a" = "a";
+  const asA: "a" = expectAOrB(param1);
+
+  const param2: "b" = "b";
+  const asB: "b" = expectAOrB(param2);
+
+  const param3: "a" | "b" = "b";
+  const asAOrB: "a" | "b" = expectAOrB(param3);
+
+  const param4: string = "b";
+  // @ts-expect-error
+  expectAOrB(param4);
 }
 
 namespace intersections {}
@@ -88,23 +93,61 @@ namespace intersections {}
 namespace tuples {}
 
 namespace functions {
-  namespace three {
+  namespace one {
     type ExpectFunctionReturningAString<F extends TODO> = F;
 
     type res1 = ExpectFunctionReturningAString<() => string>;
     type res2 = ExpectFunctionReturningAString<() => "a">;
     type res3 = ExpectFunctionReturningAString<() => "b">;
     type res4 = ExpectFunctionReturningAString<(a: number) => string>;
-    type res5 = ExpectFunctionReturningAString<(a: string) => string>;
+    type res5 = ExpectFunctionReturningAString<
+      (a: number, b: string) => string
+    >;
+    type res6 = ExpectFunctionReturningAString<(a: string) => string>;
+    // @ts-expect-error
+    type res7 = ExpectFunctionReturningAString<(a: string) => number>;
+  }
+
+  namespace two {
+    type ExpectFunctionTakingAString<F extends TODO> = F;
+
+    type res1 = ExpectFunctionTakingAString<(a: string) => void>;
+    // @ts-expect-error a function that takes only an "a" doesn't extends a function that takes a string
+    type res2 = ExpectFunctionTakingAString<(a: "a") => void>;
+    // @ts-expect-error a function that takes only an "a" doesn't extends a function that takes a string
+    type res3 = ExpectFunctionTakingAString<(a: "b") => void>;
+  }
+
+  interface SomethingThatMoves {
+    move: () => void;
+  }
+  interface Car extends SomethingThatMoves {
+    brand: string;
+    color: "red" | "green" | "blue";
+  }
+
+  interface SelfDrivingCar extends Car {
+    autopilot: () => void;
+  }
+
+  namespace three {
+    type ExpectFunctionReturningACar<F extends TODO> = F;
+
+    type res1 = ExpectFunctionReturningACar<() => Car>;
+    type res2 = ExpectFunctionReturningACar<(a: number) => SelfDrivingCar>;
+    type res3 = ExpectFunctionReturningACar<() => SelfDrivingCar>;
+    // @ts-expect-error
+    type res4 = ExpectFunctionReturningACar<() => SomethingThatMoves>;
   }
 
   namespace four {
-    type ExpectFunctionTakingAString<F extends (a: string) => any> = F;
+    type ExpectFunctionTakingACar<F extends TODO> = F;
 
-    type res1 = ExpectFunctionTakingAString<(a: string) => void>;
-    // @ts-expect-error
-    type res2 = ExpectFunctionTakingAString<(a: "a") => void>;
-    // @ts-expect-error
-    type res3 = ExpectFunctionTakingAString<(a: "b") => void>;
+    type res1 = ExpectFunctionTakingACar<(a: Car) => void>;
+    type res2 = ExpectFunctionTakingACar<(a: Car) => number>;
+    type res3 = ExpectFunctionTakingACar<(a: SomethingThatMoves) => number>;
+
+    // @ts-expect-error a function that takes only a SelfDrivingCar doesn't extends a function that takes a Car
+    type res4 = ExpectFunctionTakingACar<(a: SelfDrivingCar) => void>;
   }
 }
