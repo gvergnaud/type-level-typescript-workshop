ccbb56bc7896aa28828813a7708b6eb55013b2b6
gvergnaud <gabriel.vergnaud@gmail.com>
2022-04-14 09:05:12 +0200

    [exercise] Improve assignability exercises

--- 02-assignability/solution.ts
+++ 02-assignability/solution.ts
@@ -1,36 +1,38 @@
 /* eslint-disable  @typescript-eslint/no-namespace */
 /* eslint-disable  @typescript-eslint/no-unused-vars */
 
-import { Equal, Expect, TODO } from "../helpers";
+import { Equal, Expect } from "../helpers";
 
+// 1. strings
 namespace strings {
-  // Add a constraint on the two functions types so that it accepts only a string or only 'a'
-  function expectString<P extends string>(param: P) {
-    return param;
+  // 1.1. Add a constraint on the two functions types to make typechecking pass
+  namespace one {
+    function expectString<P extends string>(param: P) {
+      return param;
+    }
+
+    function expectHello<P extends "hello">(param: P) {
+      return param;
+    }
+
+    const res1 = (param: string) => expectString(param);
+    const res2 = (param: "hello") => expectString(param);
+    // @ts-expect-error
+    const res3 = (param: number) => expectString(param);
+
+    const res4 = (param: "hello") => expectHello(param);
+    // @ts-expect-error
+    const res5 = (param: string) => expectHello(param);
   }
-
-  function expectA<P extends "a">(param: P) {
-    return param;
-  }
-
-  const res1 = (param: string) => expectString(param);
-  const res2 = (param: "a") => expectString(param);
-  // @ts-expect-error
-  const res3 = (param: number) => expectString(param);
-
-  const res4 = (param: "a") => expectA(param);
-  // @ts-expect-error
-  const res5 = (param: string) => expectA(param);
 }
 
-// TODO objects assignability tests
+// 2. objects
 namespace objects {
+  /**
+   * 2.1. Update the type of `increment` to accept all kind of parameters
+   * which have at least a `{ count: number }` property.
+   */
   namespace one {
-    /**
-     * We would like our increment function to accept all kind of parameters
-     * which have at least a {count: number} property.
-     * Get rid of the any and type this function
-     */
     const increment = <input extends { count: number }>(
       value: input
     ): input => {
@@ -51,10 +53,11 @@ namespace objects {
     const res3 = increment({});
   }
 
+  /**
+   * 2.2. implement a `Get` generic which takes an object type
+   * and a property name and return the type of its value.
+   */
   namespace two {
-    /**
-     * Let's write a Get type that takes an object type and a named property return the type of the named property
-     */
     type Get<obj, key extends keyof obj> = obj[key];
 
     type res1 = Get<{ age: number }, "age">;
@@ -71,73 +74,102 @@ namespace objects {
   }
 }
 
+/**
+ * 3. Unions
+ */
 namespace unions {
   /**
-   * We would like our expectAOrB accept 'a' or 'b' and return the same type as passed
+   * 3.1 Update `expectCarOrBike` to accept either
+   * 'car' or 'bike' and return it's input type.
    */
-  function expectAOrB<input extends "a" | "b">(param: input) {
-    return param;
-  }
-
-  const param1: "a" = "a";
-  const asA: "a" = expectAOrB(param1);
-
-  const param2: "b" = "b";
-  const asB: "b" = expectAOrB(param2);
-
-  const param3: "a" | "b" = "b";
-  const asAOrB: "a" | "b" = expectAOrB(param3);
-
-  const param4: string = "b";
-  // @ts-expect-error
-  expectAOrB(param4);
-}
-
-namespace intersections {
-  interface User {
-    user: string;
-  }
-  interface Id {
-    id: number;
-  }
-
-  /**
-   * We would like our expectUserWithId to accept the intersection of User & Id and to return the same type as passed
-   */
-  function expectUserWithId<U extends User & Id>(param: U) {
-    return param;
-  }
-
-  const user: User = { user: "florent" };
-  const id = { id: 1234 };
-  const userWithId = { user: "florent", id: 1234 };
-
-  const userWithIdAndStatus = { user: "florent", id: 1234, status: "happy" };
-
-  expectUserWithId(userWithId);
-  // @ts-expect-error id is missing
-  expectUserWithId(user);
-  // @ts-expect-error user is missing
-  expectUserWithId(id);
-
-  // Should preserve status
-  const _: { status: string } = expectUserWithId(userWithIdAndStatus);
-}
-
-namespace tuples {
-  type ExpectTupleOfStringAnd42<F extends [string, 42]> = F;
-
-  type res1 = ExpectTupleOfStringAnd42<[string, 42]>;
-  type res2 = ExpectTupleOfStringAnd42<["literal", 42]>;
-  // @ts-expect-error number is not assignable to 42
-  type res3 = ExpectTupleOfStringAnd42<[string, number]>;
-}
-
-namespace functions {
   namespace one {
-    type ExpectFunctionReturningAString<
-      F extends (...params: any[]) => string
-    > = F;
+    function expectCarOrBike<input extends "car" | "bike">(
+      param: input
+    ): input {
+      return param;
+    }
+
+    const res1 = expectCarOrBike("car");
+    type test1 = Expect<Equal<typeof res1, "car">>;
+
+    const res2 = expectCarOrBike("bike");
+    type test2 = Expect<Equal<typeof res2, "bike">>;
+
+    const param3 = "bike" as "car" | "bike";
+    const res3 = expectCarOrBike(param3);
+    type test3 = Expect<Equal<typeof res3, "car" | "bike">>;
+
+    const param4: string = "hello";
+    // @ts-expect-error
+    expectCarOrBike(param4);
+  }
+}
+
+/// 4. Intersections
+namespace intersections {
+  /**
+   * 4.1. Update `expectUserWithId` to accept the intersection
+   * of `User` & `Id` and to return the same type as passed
+   */
+  namespace one {
+    interface User {
+      user: string;
+    }
+    interface Id {
+      id: number;
+    }
+
+    function expectUserWithId<input extends User & Id>(param: input): input {
+      return param;
+    }
+
+    const user: User = { user: "florent" };
+    const id = { id: 1234 };
+    const userWithId = { user: "florent", id: 1234 };
+
+    const userWithIdAndStatus = { user: "florent", id: 1234, status: "happy" };
+
+    expectUserWithId(userWithId);
+    // @ts-expect-error id is missing
+    expectUserWithId(user);
+    // @ts-expect-error user is missing
+    expectUserWithId(id);
+
+    // Should preserve status
+    const _: { status: string } = expectUserWithId(userWithIdAndStatus);
+  }
+}
+
+// 5. tuples
+namespace tuples {
+  // 5.1 Add the correct type constraint.
+  namespace one {
+    type ExpectTupleOfStringAnd42<F extends [string, 42]> = F;
+
+    type res1 = ExpectTupleOfStringAnd42<[string, 42]>;
+    type res2 = ExpectTupleOfStringAnd42<["literal", 42]>;
+    // @ts-expect-error number is not assignable to 42
+    type res3 = ExpectTupleOfStringAnd42<[string, number]>;
+  }
+}
+
+// 6. functions
+namespace functions {
+  interface SomethingThatMoves {
+    move: () => void;
+  }
+  interface Car extends SomethingThatMoves {
+    brand: string;
+    color: "red" | "green" | "blue";
+  }
+
+  interface SelfDrivingCar extends Car {
+    autopilot: () => void;
+  }
+
+  // 6.1. Add the correct type constraint.
+  namespace one {
+    type ExpectFunctionReturningAString<F extends (...args: any) => string> = F;
 
     type res1 = ExpectFunctionReturningAString<() => string>;
     type res2 = ExpectFunctionReturningAString<() => "a">;
@@ -151,8 +183,9 @@ namespace functions {
     type res7 = ExpectFunctionReturningAString<(a: string) => number>;
   }
 
+  // 6.2. Add the correct type constraint.
   namespace two {
-    type ExpectFunctionTakingAString<F extends (str: string) => any> = F;
+    type ExpectFunctionTakingAString<F extends (a: string) => any> = F;
 
     type res1 = ExpectFunctionTakingAString<(a: string) => void>;
     // @ts-expect-error a function that takes only an "a" doesn't extends a function that takes a string
@@ -161,20 +194,9 @@ namespace functions {
     type res3 = ExpectFunctionTakingAString<(a: "b") => void>;
   }
 
-  interface SomethingThatMoves {
-    move: () => void;
-  }
-  interface Car extends SomethingThatMoves {
-    brand: string;
-    color: "red" | "green" | "blue";
-  }
-
-  interface SelfDrivingCar extends Car {
-    autopilot: () => void;
-  }
-
+  // 6.3. Add the correct type constraint.
   namespace three {
-    type ExpectFunctionReturningACar<F extends (...params: any[]) => Car> = F;
+    type ExpectFunctionReturningACar<F extends (...args: any) => Car> = F;
 
     type res1 = ExpectFunctionReturningACar<() => Car>;
     type res2 = ExpectFunctionReturningACar<(a: number) => SelfDrivingCar>;
@@ -183,8 +205,9 @@ namespace functions {
     type res4 = ExpectFunctionReturningACar<() => SomethingThatMoves>;
   }
 
+  // 6.4. Add the correct type constraint.
   namespace four {
-    type ExpectFunctionTakingACar<F extends (car: Car) => any> = F;
+    type ExpectFunctionTakingACar<F extends (a: Car) => any> = F;
 
     type res1 = ExpectFunctionTakingACar<(a: Car) => void>;
     type res2 = ExpectFunctionTakingACar<(a: Car) => number>;
