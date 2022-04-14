cecae14cac4ec48c456cf8e8c1a1fc0dde95897a
gvergnaud <gabriel.vergnaud@gmail.com>
2022-04-14 09:24:45 +0200

    [exercise] Update comment formatting

--- 01-the-data/exercise.ts
+++ 01-the-data/exercise.ts
@@ -3,7 +3,9 @@
 
 import { Equal, Expect, TODO } from "../helpers";
 
-// 1. implement a generic to get the union of all keys of an object type.
+/**
+ * 1. implement a generic to get the union of all keys of an object type.
+ */
 namespace one {
   type KeyOf<obj> = TODO;
 
@@ -20,7 +22,9 @@ namespace one {
   type test4 = Expect<Equal<res4, string>>;
 }
 
-// 2. implement a generic to get the union of all values in an object type.
+/**
+ * 2. implement a generic to get the union of all values in an object type.
+ */
 namespace two {
   type ValueOf<obj> = TODO;
 
@@ -37,11 +41,15 @@ namespace two {
   type test4 = Expect<Equal<res4, boolean>>;
 }
 
-// 3. implement a generic to get all types inside a tuple.
+/**
+ * 3. implement a generic to get all types inside a tuple.
+ */
 namespace three {
-  // The `extends any[]` part is what we call a "type constraint".
-  // We haven't covered constraints yet, but we will in the next chapter.
-  // For now, just implement the type by replacing the TODO
+  /**
+   * The `extends any[]` part is what we call a "type constraint".
+   * We haven't covered constraints yet, but we will in the next chapter.
+   * For now, just implement the type by replacing the TODO
+   */
   type ValueOf<tuple extends any[]> = TODO;
 
   type res1 = ValueOf<[number, string]>;
@@ -57,7 +65,9 @@ namespace three {
   type test4 = Expect<Equal<res4, boolean>>;
 }
 
-// 4. Implement a generic which gets the length of a tuple type.
+/**
+ * 4. Implement a generic which gets the length of a tuple type.
+ */
 namespace four {
   type Length<tuple extends any[]> = TODO;
 
