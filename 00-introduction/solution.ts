5faf710932e52e1eb71e424d284f54f1fc450fb2
Shinya Fujino <shf0811@gmail.com>
2022-10-10 18:27:47 +0900

    Fix a minor indentation problem

--- 00-introduction/solution.ts
+++ 00-introduction/solution.ts
@@ -5,7 +5,7 @@ import { Equal, Expect } from "../helpers";
 /**
  * 0. Warmup, try assigning "World" to `type Hello`.
  */
- namespace zero {
+namespace zero {
   type Hello = "World";
 
   type test1 = Expect<Equal<Hello, "World">>;
